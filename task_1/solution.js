46594752362772ec797077ea833d548ebbd941e8
shibkisa <kolakid111@gmail.com>
2021-11-15 22:26:26 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,19 +1,18 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
+    let message = PruductName;
+   
     // Задание №1.1. Оповещение о добавлении в корзину
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // Комментарий
-    // выведите переменную message в консоль
+    let message;
+    let message = message + " ";
+    let message = message + "за";
+    let message = message + " ";
+    let massage + productPrice;
+    let message = message + " ";
+    let message = message + "теперь в корзине!";
+    let console.log(message);
 
     // Конец решения задания №1.1.
 
@@ -25,9 +24,8 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+    let newValue = oldValue;
+    let newValue = newValue+1;
 
     // Конец решения задания №1.2.
 
@@ -40,9 +38,8 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSum = oldSum + difference; 
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
