f8a637a4390a303544afd1593e68b62aeda82164
shibkisa <kolakid111@gmail.com>
2021-11-16 20:04:39 +0300

    Task 1.5 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -6,13 +6,13 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     let message;
-    message = "productName";
-    message = message + " ";
-    message = message + "за";
-    message = message + " ";
+    message = 'productName';
+    message = message + ' ';
+    message = message + 'за';
+    message = message + ' ';
     message + productPrice;
-    message = message + " ";
-    message = message + "теперь в корзине!";
+    message = message + ' ';
+    message = message + 'теперь в корзине!';
       console.log(message);
 
  // Конец решения задания №1.1.
