5ee2f5e95cda04724c799ecefaec5ee6e2890cde
shibkisa <kolakid111@gmail.com>
2021-11-17 22:22:37 +0300

    2.

--- task_2/solution.js
+++ task_2/solution.js
@@ -27,7 +27,7 @@ function calcDiscount(sum, min, discount) {
     let discountSum; // создайте переменную discountSum
 
      if (productsSum >= discountMinSum) { // если productsSum больше или равно discountMinSum,
-        discountSum = productsSum * (0,01 * discountPart) ; // то присвойте discountSum значение discountPart процентов от productsSum,
+        discountSum = productsSum *  discountPart ; // то присвойте discountSum значение discountPart процентов от productsSum,
     }else{
         discountSum = 0; // иначе присвойте discountSum значение 0
     }
