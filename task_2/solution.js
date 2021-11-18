4eccbe10db499688ab90b2c3bc2fd9a2a2277c61
shibkisa <kolakid111@gmail.com>
2021-11-18 19:57:35 +0300

    Task 2.3 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,15 +6,19 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 	
     let shippingSum;
+
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
       } 
     if (productsSum > 0 && productsSum < freeShippingMinSum){
         shippingSum = shippingPrice;
     }
+
     // Конец решения задания №2.1.
     return shippingSum;
 }
+
+
 function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
@@ -22,11 +26,12 @@ function calcDiscount(sum, min, discount) {
 	
     // Задание №2.2. Рассчитать скидку
 
-	let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0;
+	let discountSum = productsSum >= productsSum / 100 * discountPart : 0;
     }
     // Конец решения задания №2.2.
     return discountSum;
 }
+
 
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
@@ -37,12 +42,15 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let totalSum;
     totalSum = productsSum;
     totalSum = totalSum - discountSum;
+
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice);
-    totalSum = totalSum + shippingSum;
+
     let freeShipping;
+    totalSum = totalSum + shippingSum;
     freeShipping = !shippingSum;
 	
     // Конец решения задачи №2.3.
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
 }
+
 
