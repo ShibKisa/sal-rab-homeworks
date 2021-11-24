d284838315c90c177b2aec201032ac17f69d6872
shibkisa <kolakid111@gmail.com>
2021-11-24 19:34:43 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,6 +38,7 @@ function sendRequest(name, phone, address, goods, sum) {
   let data = {client "",  order: {}, goods: []};
   
   let countOfGoods = goods.length;
+  
   for (let i = 0; i < countOfGoods; i += 1) {
   data.goods.push({title: goods[i].title, count: goods[i].count});
   };
@@ -46,6 +47,6 @@ function sendRequest(name, phone, address, goods, sum) {
   data.order.sum = sum;
   data.client = name + " " + phone;
 
-  let jsonData = JSON.stringify({data: data});
+  let jsonData = JSON.stringify({data});
   return jsonData;
  }
